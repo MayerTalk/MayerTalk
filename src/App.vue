95d943046e2f1c5c6172e1bf709235246ed6b6f0
meeboo3 <meeboo3@qq.com>
2022-11-27 23:28:49 +0800

    添加测试数据

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
     import {ref, provide} from 'vue'
     import Renders from './render'
     import Announce from './Announce.vue'
+    import Test from './Test.vue'
 
     const showAnnouncement = ref(false);
     const config = ref({
         render: 'Arknights',
         style: 'default',
-        background: null
+        settings: {}
     });
     const chars = ref({});
     const chats = ref([]);
+
+
 
     provide('showAnnouncement', showAnnouncement);
     provide('config', config);
@@ -19,6 +22,7 @@
 </script>
 
 <template>
+    <Test/>
     <Announce v-model="showAnnouncement"/>
     <component :is="Renders[config.render]"/>
 </template>
