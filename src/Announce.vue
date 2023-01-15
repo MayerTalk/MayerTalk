b9c51f3db24b460ec20c7276bef23fe9a49e3144
meeboo3 <meeboo3@qq.com>
2023-01-15 13:32:01 +0800

    update v0.1.0

--- src/Announce.vue
+++ src/Announce.vue
@@ -15,10 +15,8 @@
     });
     const ifShowUnsupportBrowser = ref(false);
 
-    const version = 'v0.0.5';
+    const version = 'v0.1.0';
     const dialogWidth = Math.ceil(Math.min(document.body.clientWidth, 700) * 0.9);
-
-    const isNewSite = location.href.indexOf('https://www.mayertalk.top/') === 0;
 
     onMounted(() => {
         if (getData('a_version') !== version) {
@@ -34,16 +32,25 @@
         <p>开发阶段，功能尚不完善，还请谅解</p>
         <p><b>所有内容均在本地生成，不会上传至服务器，不具备云端保存功能</b></p>
         <h3>
-            v0.0.5
+            v0.1.0
         </h3>
-        - 支持 撤销/重做<br/>
-        - 新增对话类型 选项/选择/标题<br/>
-        - 新增渲染设置<br/>
-        - 修复移动端元素溢出<br/>
-        -
-        <el-link href="https://www.mayertalk.top/" type="primary">
-            {{isNewSite?'站点迁移 (您已在新站点)':'站点迁移 前往[https://www.mayertalk.top/]'}}
-        </el-link>
+        <b>新增</b>
+        <ul>
+            <li>@提示</li>
+            <li>+1</li>
+            <li>复读</li>
+            <li>仅清空对话</li>
+            <li>清空本地数据(设置)</li>
+            <li>Ctrl + C 快捷创建橘色</li>
+        </ul>
+        <b>修复</b>
+        <ul>
+            <li>长对话编辑时错误转跳</li>
+        </ul>
+        <b>优化</b>
+        <ul>
+            <li>角色栏由横向滚动改为纵向</li>
+        </ul>
         <h3>关于截图失败</h3>
         <b>iOS：待支持</b><br/>
         <b>安卓：推荐使用via</b><br/>
@@ -54,6 +61,7 @@
             <el-link @click="$emit('showGuide', false)" href="javascript:void(0)">查看指南</el-link>
         </p>
         <el-link href="https://jq.qq.com/?_wv=1027&k=ImatbCzG" type="primary">交流群：560295639</el-link>
+        <el-link href="https://wj.qq.com/s2/11537223/aa61/" type="primary" style="margin-left: 10px">bug反馈</el-link>
         <div style="position: absolute; bottom: 0; right: 0; color: #EEEEEE">咕咕</div>
     </el-dialog>
     <el-dialog v-model="ifShowUnsupportBrowser" title="不支持的浏览器" :width="dialogWidth">
@@ -65,3 +73,9 @@
         </ul>
     </el-dialog>
 </template>
+
+<style scoped>
+    ul {
+        margin: 5px 0;
+    }
+</style>
