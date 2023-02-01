9ad9f6c355152f93108a6df5b013ef49a69a0b08
meeboo3 <meeboo3@qq.com>
2023-02-01 21:32:00 +0800

    optimize 手机端创建角色后不自动focus textarea

--- src/render/arknights/Render.vue
+++ src/render/arknights/Render.vue
@@ -560,7 +560,9 @@
             ifShowEditChar.value = false;
             newChar.value = {name: ''};
             message.notify('创建成功', message.success);
-            document.getElementById('textarea').focus();
+            if (!toolBarMask.value) {
+                document.getElementById('textarea').focus();
+            }
         } else {
             message.confirm(
                 '即将删除该角色',
