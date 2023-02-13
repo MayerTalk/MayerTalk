39f01fcbd3cea8dfe5391c0fa183b567eda98753
meeboo3 <meeboo3@qq.com>
2023-02-14 00:12:41 +0800

    update announce

--- src/Announce.vue
+++ src/Announce.vue
@@ -22,37 +22,58 @@
         if (getData('a_version') !== version) {
             emit('update:modelValue', true);
             saveData('a_version', version)
+        } else if (invalidBrowser) {
+            emit('update:modelValue', true);
         }
     });
+
+    const invalidBrowser = navigator.userAgent.indexOf('UCBrowser') !== -1
+        || navigator.userAgent.indexOf('quark') !== -1
+        || navigator.userAgent.indexOf('QQBrowser') !== -1
+        || navigator.userAgent.indexOf('baidubrowser') !== -1;
 </script>
 
 <template>
     <el-dialog v-model="ifShowAnnouncement" :title="'公告 ' + version" :width="dialogWidth">
         <h2 style="display: inline">MayerTalk(beta)</h2>
         <p>开发阶段，功能尚不完善，还请谅解</p>
-        <p><b>所有内容均在本地生成，不会上传至服务器，不具备云端保存功能</b></p>
-        <el-link href="/docs/guide/start.html" type="primary">快速上手 (首次使用推荐阅读)</el-link>
-        <h3>
-            v0.1.3
-        </h3>
-        <b>新增</b>
-        <ul>
-            <li>
-                支持别名搜索<br/>
-                e.g. 现在可以用“刁民”搜索“乌萨斯平民”了<br/>
-                <p style="display: flex; margin: 0">没有想要的别名？
-                    <el-link href="https://alias.arkfans.top/docs/alias.html" type="primary" style="margin-left: 5px;" target="_blank">
-                        点我提交
-                    </el-link>
-                </p>
-            </li>
-        </ul>
-        <h3>关于截图失败</h3>
-        <b>iOS：待支持</b><br/>
-        <b>安卓：推荐使用via</b><br/>
-        <el-link @click="ifShowUnsupportBrowser=true" href="javascript:void(0)">查看不支持的浏览器</el-link>
-        <br/>
-        <el-link href="https://wj.qq.com/s2/11287516/2689/" type="primary">其他浏览器无法截图/导出反馈</el-link>
+        <template v-if="invalidBrowser">
+            <h2 style="color: red">
+                您当前使用的浏览器无法导出截图，请使用其他浏览器
+            </h2>
+            <h3>Q：为什么？</h3>
+            <h3>A：您当前使用的浏览不支持blob，而为了在本地导出截图，blob是必须的</h3>
+            <h3>Q：什么浏览器支持blob？</h3>
+            <h3>A：推荐Edge/Chrome</h3>
+        </template>
+        <template v-else>
+            <p><b>所有内容均在本地生成，不会上传至服务器，不具备云端保存功能</b></p>
+            <el-link href="/docs/guide/start.html" type="primary">快速上手 (首次使用推荐阅读)</el-link>
+            <h3>
+                v0.1.3
+            </h3>
+            <b>新增</b>
+            <ul>
+                <li>
+                    支持别名搜索<br/>
+                    e.g. 现在可以用“刁民”搜索“乌萨斯平民”了<br/>
+                    <p style="display: flex; margin: 0">没有想要的别名？
+                        <el-link href="https://alias.arkfans.top/docs/alias.html" type="primary"
+                                 style="margin-left: 5px;"
+                                 target="_blank">
+                            点我提交
+                        </el-link>
+                    </p>
+                </li>
+            </ul>
+            <h3>关于截图失败</h3>
+
+            <b>iOS：待支持</b><br/>
+            <b>安卓：推荐使用via</b><br/>
+            <el-link @click="ifShowUnsupportBrowser=true" href="javascript:void(0)">查看不支持的浏览器</el-link>
+            <br/>
+            <el-link href="https://wj.qq.com/s2/11287516/2689/" type="primary">其他浏览器无法截图/导出反馈</el-link>
+        </template>
         <div style="display: flex; margin-top: 10px">
             <el-link href="https://jq.qq.com/?_wv=1027&k=ImatbCzG" type="primary" style="margin-right: 5px">
                 交流群：560295639
