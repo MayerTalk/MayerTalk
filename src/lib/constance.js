d415882017ef5c0d8d4e28e2e056e69dde09fe90
meeboo3 <meeboo3@qq.com>
2023-03-05 02:38:12 +0800

    update 弃用genStyle

--- src/lib/constance.js
+++ src/lib/constance.js
@@ -21,7 +21,7 @@ const TypeDefault = {
 }
 
 const defaultWidth = 520
-// #22 后 document.body.clientWidth 比实际视窗小（Why？）
+// #21 后 document.body.clientWidth 比实际视窗小（Why？）
 const windowWidth = Math.min(defaultWidth, window.innerWidth)
 const dialogWidth = Math.ceil(windowWidth * 0.9)
 const MobileView = !(window.innerWidth - defaultWidth > 250)
